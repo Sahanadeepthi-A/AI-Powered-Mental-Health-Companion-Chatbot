d6e2161b5d3fac3f4a74b39077149c5d11f4dafd
Sahanadeepthi-A <160331005+Sahanadeepthi-A@users.noreply.github.com>
2026-01-21 16:49:09 +0530

    Refactor model loading paths in explain.py
    Refactor file paths for model loading to use BASE_DIR.

--- backend/explain.py
+++ backend/explain.py
@@ -1,21 +1,24 @@
-import joblib
+import os
 import joblib
 import numpy as np
 
-explainer = joblib.load("model/shap_explainer.pkl")
-vectorizer = joblib.load("model/vectorizer.pkl")
-label_encoder = joblib.load("model/label_encoder.pkl")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+EXPLAINER_PATH = os.path.join(BASE_DIR, "model", "shap_explainer.pkl")
+VECTORIZER_PATH = os.path.join(BASE_DIR, "model", "vectorizer.pkl")
+ENCODER_PATH = os.path.join(BASE_DIR, "model", "label_encoder.pkl")
+
+explainer = joblib.load(EXPLAINER_PATH)
+vectorizer = joblib.load(VECTORIZER_PATH)
+label_encoder = joblib.load(ENCODER_PATH)
 
 def explain(text, predicted_label):
     vec = vectorizer.transform([text])
     vec_dense = vec.toarray()
 
     shap_values = explainer(vec_dense)
-
-    # Get class index for the predicted label
     class_index = list(label_encoder.classes_).index(predicted_label)
 
-    # Extract SHAP values for that class
     shap_for_class = shap_values[..., class_index]
+    return shap_for_class
 
-    return shap_for_class, vec_dense
